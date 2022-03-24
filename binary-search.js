7d5119edc0bc4e1ea386132dd919b123c3e034e2
lemmyosborne <bozhenko.rodion@mail.ru>
2022-03-24 06:42:30 +0300

    first commit

---
+++ binary-search.js
@@ -0,0 +1,21 @@
+"use strict";
+const binarySearch = (array, target) => {
+    let low = 0;
+    let high = array.length - 1;
+    let middle = Math.ceil((high + low) / 2);
+    while (low <= high) {
+        middle = Math.ceil((high + low) / 2);
+        let guess = array[middle];
+        if (guess === target)
+            return middle;
+        if (guess > target) {
+            high = middle - 1;
+        }
+        else {
+            low = middle + 1;
+        }
+    }
+    return null;
+};
+const arr = [1, 4, 5, 6, 7, 8, 9, 10, 100, 200, 444];
+console.log(binarySearch(arr, 444));
---
+++ binary-search.ts
@@ -0,0 +1,24 @@
+const binarySearch = (array: number[], target: number) => {
+  let low = 0;
+  let high: number = array.length - 1;
+  let middle = Math.ceil((high + low) / 2);
+
+  while (low <= high) {
+    middle = Math.ceil((high + low) / 2);
+    let guess = array[middle];
+
+    if (guess === target) return middle;
+
+    if (guess > target) {
+      high = middle - 1;
+    } else {
+      low = middle + 1;
+    }
+  }
+
+  return null;
+};
+
+const arr = [1, 4, 5, 6, 7, 8, 9, 10, 100, 200, 444];
+
+console.log(binarySearch(arr, 444));
